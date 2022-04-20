7881f864e3f725ebd154c7236518493edc174d2a
thalia <fabio.madeira.pro@gmail.com>
2022-04-20 17:10:27 +0200

    Apparence du simulateur terminé

--- navigation.js
+++ navigation.js
@@ -4,12 +4,24 @@ class simNavigation {
         this.slides = document.querySelectorAll('.sim-slide').length
     }
 
-    onNavigationBack() {
-        this.navigation--
+    onNavigationBack(answers) {
+
+        if(answers.shop == false && this.navigation == 7) {
+            this.navigation = 3
+        } else {
+            this.navigation--
+        }
+
     }
 
-    onNavigationForward() {
-        this.navigation++
+    onNavigationForward(answers) {
+        
+        if(answers.shop == false && this.navigation == 3) {
+            this.navigation = 7
+        } else {
+            this.navigation++
+        }
+
     }
 
     onSetNavigation(key) {
--- script.js
+++ script.js
@@ -1,4 +1,38 @@
 document.addEventListener("DOMContentLoaded", () => { 
+
+    let answers = new sim_Simulator()
+
+    // CREATION OU REFONTE
+    sim_setChoice('.sim-answer-creation', (data) => answers.setWork(data))
+    // PAGES
+    sim_setRange('.sim-answer-pages', () => answers.setPage())
+    // SEO
+    sim_setChoice('.sim-answer-seo', (data) => answers.setSeo(data))
+    // SHOP
+    sim_setChoice('.sim-answer-shop', (data) => answers.setShop(data))
+    // CLIENT
+    sim_setChoice('.sim-answer-client', (data) => answers.setClientBoard(data))
+    // CAT
+    sim_setRange('.sim-answer-cat', () => answers.setCatPages())
+    // PROD
+    sim_setRange('.sim-answer-prod', () => answers.setProdPage())
+    // CAT SEO
+    sim_setChoice('.sim-answer-cat-seo', (data) => answers.setWriteCatPages(data))
+    // PROD SEO
+    sim_setChoice('.sim-answer-prod-seo', (data) => answers.setWriteProdPage(data))
+    // BLOG
+    sim_setChoice('.sim-answer-blog', (data) => answers.setBlog(data))
+    // GRAPHIC
+    sim_setChoice('.sim-answer-graphic', (data) => answers.setGraphic(data))
+    // LANG
+    sim_setMultiple('.sim-answer-english', () => answers.toggleEnglish())
+    sim_setMultiple('.sim-answer-spanish', () => answers.toggleSpanish())
+    sim_setMultiple('.sim-answer-italian', () => answers.toggleItalian())
+    sim_setMultiple('.sim-answer-deutsch', () => answers.toggleDeutsch())
+    sim_setMultiple('.sim-answer-netherlands', () => answers.toggleNetherlands())
+    sim_setMultiple('.sim-answer-portuguese', () => answers.togglePortuguese())
+
+
     let nav = new simNavigation()
 
     sim_getDots(nav)
@@ -7,7 +41,7 @@ document.addEventListener("DOMContentLoaded", () => {
     document.querySelectorAll(".sim-qst-action-before").forEach(item => {
         item.addEventListener('click', () => {
             if(nav.navigation > 0) {
-                nav.onNavigationBack()
+                nav.onNavigationBack(answers)
                 document.querySelectorAll('.sim-slide').forEach(slide => {
                     slide.style.transform = `translateX(-${nav.navigation}00%)`
                     sim_getDots(nav)
@@ -20,7 +54,7 @@ document.addEventListener("DOMContentLoaded", () => {
     document.querySelectorAll(".sim-qst-action-next").forEach(item => {
         item.addEventListener('click', () => {
             if(nav.navigation >= 0 && nav.navigation <= nav.slides) {
-                nav.onNavigationForward()
+                nav.onNavigationForward(answers)
                 document.querySelectorAll('.sim-slide').forEach(slide => {
                     slide.style.transform = `translateX(-${nav.navigation}00%)`
                     sim_getDots(nav)
@@ -46,6 +80,130 @@ function sim_getDots(nav){
         document.querySelector('.sim-navigation-row').innerHTML += `
             <button data-key="${i}" class="${className}"></button>
         `
+    }
+
+}
+
+function sim_setChoice(theClass, theFunction) {
+    document.querySelectorAll(theClass).forEach(item => {
+        item.addEventListener('click', () => {
+                
+            theFunction(item.getAttribute('data-value') == 'y' ? true : false)
+
+            document.querySelectorAll(theClass).forEach(i => {
+                if(item != i) {
+                    i.classList.remove('sim-qst-content-answer-active')
+                }
+            })
+            item.classList.add('sim-qst-content-answer-active')
+        })
+    })
+}
+
+function sim_setRange(theClass, theFunction) {
+    document.querySelector(theClass).addEventListener('input', () => {
+        let value = this.event.target.value
+        this.event.target.closest('.sim-qst-content').querySelector('.sim-qst-content-range-value').textContent = value
+        theFunction(value)
+    })
+}
+
+function sim_setMultiple(theClass, theFunction) {
+    document.querySelectorAll(theClass).forEach(item => {
+        item.addEventListener('click', () => {
+            theFunction()
+            item.classList.contains('sim-qst-content-answer-active') ? item.classList.remove('sim-qst-content-answer-active') : item.classList.add('sim-qst-content-answer-active')
+        })
+    })
+}
+
+class sim_Simulator {
+    constructor(){
+        this.work = null // Création || Refonte
+        this.page = 1 // Nombre de page
+        this.seo = null // type d'analyse sémantique
+        this.shop = null // E-commerce
+        this.clientBoard = null // Espace client
+        this.catPages = 0 // Nombre de page catégories
+        this.prodPage = 0 // Nombre de page produits
+        this.writeCatPages = null // Rédaction des pages catégories
+        this.writeProdPage = null // Rédaction des pages produits
+        this.blog = null // Blog
+        this.graphic = false // Charte Graphique
+        this.english = false
+        this.spanish = false
+        this.italian = false
+        this.deutsch = false
+        this.netherlands = false
+        this.portuguese = false
+    }
+
+    setWork(value) {
+        this.work = value
+    }
+
+    setPage(value) {
+        this.page = value
+    }
+
+    setSeo(value) {
+        this.seo = value
+    }
+
+    setShop(value) {
+        this.shop = value
+    }
+
+    setClientBoard(value) {
+        this.clientBoard = value
+    }
+
+    setCatPages(value) {
+        this.catPages = value
+    }
+
+    setProdPage(value) {
+        this.prodPage = value
+    }
+
+    setWriteCatPages(value) {
+        this.writeCatPages = value
+    }
+
+    setWriteProdPage(value) {
+        this.writeProdPage = value
+    }
+
+    setBlog(value) {
+        this.blog = value
+    }
+
+    setGraphic(value) {
+        this.graphic = value
+    }
+
+    toggleEnglish() {
+        this.english = !this.english
+    }
+
+    toggleSpanish() {
+        this.spanish = !this.spanish
+    }
+
+    toggleItalian() {
+        this.italian = !this.italian
+    }
+
+    toggleDeutsch() {
+        this.deutsch = !this.deutsch
+    }
+
+    toggleNetherlands() {
+        this.netherlands = !this.netherlands
+    }
+
+    togglePortuguese() {
+        this.portuguese = !this.portuguese
     }
 
 }
